0362f6d4a8b79b738993ec45c25ffb157dd9cfd0
sanjana-shetty24 <sanjanav.cd24@bmsce.ac.in>
2026-07-21 17:07:57 +0530

    Use VITE_API_URL for deployed backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,39 +1,39 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 60000,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
-})
+});
 
 // Create a new chat
-export const createNewChat = (title = 'New Chat') =>
-  api.post('/new-chat', { title }).then((res) => res.data)
+export const createNewChat = (title = "New Chat") =>
+  api.post("/new-chat", { title }).then((res) => res.data);
 
 // Send a message
 export const sendMessage = (chat_id, message) =>
-  api.post('/chat', { chat_id, message }).then((res) => res.data)
+  api.post("/chat", { chat_id, message }).then((res) => res.data);
 
 // Get chat history
 export const fetchHistory = () =>
-  api.get('/history').then((res) => res.data)
+  api.get("/history").then((res) => res.data);
 
 // Get a single chat
 export const fetchChat = (chatId) =>
-  api.get(`/chat/${chatId}`).then((res) => res.data)
+  api.get(`/chat/${chatId}`).then((res) => res.data);
 
 // Delete one chat
 export const deleteChat = (chatId) =>
-  api.delete(`/chat/${chatId}`).then((res) => res.data)
+  api.delete(`/chat/${chatId}`).then((res) => res.data);
 
 // Rename a chat
 export const renameChat = (chatId, title) =>
-  api.patch(`/chat/${chatId}/rename`, { title }).then((res) => res.data)
+  api.patch(`/chat/${chatId}/rename`, { title }).then((res) => res.data);
 
 // Delete all chats
 export const clearAllChats = () =>
-  api.delete('/chats/all').then((res) => res.data)
+  api.delete("/chats/all").then((res) => res.data);
 
-export default api
+export default api;
